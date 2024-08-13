2a49453499335f76829e467a812805c3c3f29d1a
SHEDESIGNS <chichey@yahoo.com>
2024-08-13 13:55:20 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Function to show the welcome message
+function showWelcome() {
+    document.getElementById("welcome-message").style.display = "block";
+}
+
+// Call the showWelcome function when the page loads
+window.onload = showWelcome;
+
